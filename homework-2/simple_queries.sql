53d8e06a3d7d3a625a850b94c2d6441b4cc33892
Iaroslav <yaroslav.da@mail.ru>
2024-02-13 22:47:38 +0300

    add filter sorting and fix simple queries

--- homework-2/simple_queries.sql
+++ homework-2/simple_queries.sql
@@ -3,7 +3,7 @@
 SELECT contact_name, city FROM customers
 
 -- 2. идентификатор заказа и разницу между датами формирования (order_date) заказа и его отгрузкой (shipped_date) из таблицы orders
-SELECT order_id, order_date - shipped_date as delta_time FROM orders
+SELECT order_id, shipped_date - order_date as delta_time FROM orders
 
 -- 3. все города без повторов, в которых зарегистрированы заказчики (customers)
 SELECT DISTINCT city FROM customers
